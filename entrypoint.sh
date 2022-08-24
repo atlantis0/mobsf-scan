89de970a89d3ae34f384b320fd1d3803c398d6bf
Sirack Hailu <sirackh@gmail.com>
2022-08-24 12:24:51 +0300

    simplified entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,9 @@ fi
 
 echo ${GITHUB_WORKSPACE} 
 which mobsfscan
+file $(which mobsfscan)
 # mobsfscan ${GITHUB_WORKSPACE} --json --output ${OUTPUT_FILE_NAME}
-mobsfscan /github/workspace --json --output ${OUTPUT_FILE_NAME}
-cat ${OUTPUT_FILE_NAME}
+# mobsfscan /github/workspace --json --output ${OUTPUT_FILE_NAME}
+# cat ${OUTPUT_FILE_NAME}
 
 # docker run -v /home/pwned_report/mobsf:/src -e SRC_DIR='/src' -e OUTPUT_FILE_NAME='/result.json' mobsfscan
