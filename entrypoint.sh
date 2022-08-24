d3e973b570fe56a4f04193d717a3ea335ea37e84
Sirack Hailu <sirackh@gmail.com>
2022-08-24 11:32:06 +0300

    src dir point to GITHUB_WORKSPACE env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 
 #!/bin/bash
 
-if [ -z "$SRC_DIR" ]; then
-  echo "SRC_DIR is required to run MobSF scan action. (SRC_DIR = $SRC_DIR)"
-  exit 126
-fi
+# if [ -z "$SRC_DIR" ]; then
+#   echo "SRC_DIR is required to run MobSF scan action. (SRC_DIR = $SRC_DIR)"
+#   exit 126
+# fi
 
 if [ -z "$OUTPUT_FILE_NAME" ]; then
   echo "OUTPUT_FILE_NAME is required to run MobSF scan action. (OUTPUT_FILE_NAME = $OUTPUT_FILE_NAME)"
   exit 126
 fi
 
-mobsfscan ${SRC_DIR} --json --output ${OUTPUT_FILE_NAME}
+mobsfscan ${GITHUB_WORKSPACE} --json --output ${OUTPUT_FILE_NAME}
 cat ${OUTPUT_FILE_NAME}
 
 # docker run -v /home/pwned_report/mobsf:/src -e SRC_DIR='/src' -e OUTPUT_FILE_NAME='/result.json' mobsfscan
