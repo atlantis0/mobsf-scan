e7b5d206797f0c6a731c527af4074142b18010c8
Sirack Hailu <sirackh@gmail.com>
2022-08-25 11:22:57 +0300

    empty entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,14 @@
 #   exit 126
 # fi
 
-if [ -z "$OUTPUT_FILE_NAME" ]; then
-  echo "OUTPUT_FILE_NAME is required to run MobSF scan action. (OUTPUT_FILE_NAME = $OUTPUT_FILE_NAME)"
-  exit 126
-fi
+# if [ -z "$OUTPUT_FILE_NAME" ]; then
+#   echo "OUTPUT_FILE_NAME is required to run MobSF scan action. (OUTPUT_FILE_NAME = $OUTPUT_FILE_NAME)"
+#   exit 126
+# fi
 
-echo ${GITHUB_WORKSPACE} 
-which mobsfscan
-file $(which mobsfscan)
+# echo ${GITHUB_WORKSPACE} 
+# which mobsfscan
+# file $(which mobsfscan)
 # mobsfscan ${GITHUB_WORKSPACE} --json --output ${OUTPUT_FILE_NAME}
 # mobsfscan /github/workspace --json --output ${OUTPUT_FILE_NAME}
 # cat ${OUTPUT_FILE_NAME}
