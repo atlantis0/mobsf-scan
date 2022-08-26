2db1b67d19610bdac1aa55eace9c2890f04a2d91
Sirack Hailu <sirackh@gmail.com>
2022-08-26 12:04:14 +0300

    added format as arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 
 #!/bin/bash
 
-# if [ -z "$SRC_DIR" ]; then
-#   echo "SRC_DIR is required to run MobSF scan action. (SRC_DIR = $SRC_DIR)"
-#   exit 126
-# fi
+if [ -z "$FORMAT" ]; then
+  echo "FORMAT is required to run MobSF scan action. (FORMAT = $FORMAT)"
+  exit 126
+fi
 
 if [ -z "$OUTPUT_FILE_NAME" ]; then
   echo "OUTPUT_FILE_NAME is required to run MobSF scan action. (OUTPUT_FILE_NAME = $OUTPUT_FILE_NAME)"
@@ -15,9 +15,5 @@ echo ${GITHUB_WORKSPACE}
 which mobsfscan
 echo ${GITHUB_WORKSPACE}/${OUTPUT_FILE_NAME}
 ls -la ${GITHUB_WORKSPACE}
-mobsfscan ${GITHUB_WORKSPACE} --json --output ${GITHUB_WORKSPACE}/${OUTPUT_FILE_NAME}
+mobsfscan ${GITHUB_WORKSPACE} --${FORMAT} --output ${GITHUB_WORKSPACE}/${OUTPUT_FILE_NAME}
 ls -la ${GITHUB_WORKSPACE}/${OUTPUT_FILE_NAME}
-# mobsfscan /github/workspace --json --output ${OUTPUT_FILE_NAME}
-# cat ${OUTPUT_FILE_NAME}
-
-# docker run -v /home/pwned_report/mobsf:/src -e SRC_DIR='/src' -e OUTPUT_FILE_NAME='/result.json' mobsfscan
